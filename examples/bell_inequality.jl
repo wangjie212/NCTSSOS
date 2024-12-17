2f440d5ee5bb5f86ad466c317857d5ef7c9153b2
jwang <wangjie212@mails.ucas.ac.cn>
2024-12-17 17:21:35 +0800

    add support for normality conditions and sohs programming

--- examples/bell_inequality.jl
+++ examples/bell_inequality.jl
@@ -1,9 +1,10 @@
 using DynamicPolynomials
 using NCTSSOS
 
+
+# I_3322 inequality
 @ncpolyvar x[1:3]
 @ncpolyvar y[1:3]
 f = x[1]*(y[1] + y[2] + y[3]) + x[2]*(y[1] + y[2] - y[3]) + x[3]*(y[1] - y[2]) - x[1] - 2*y[1] - y[2]
 
-opt,data = nctssos_first([-f], [x;y], 4, TS=false, normality=true, partition=3, constraint="projection")
-opt,data = nctssos_higher!(data, TS="block")
+opt,data = nctssos_first([-f], [x;y], 3, TS=false, normality=true, partition=3, constraint="projection")
