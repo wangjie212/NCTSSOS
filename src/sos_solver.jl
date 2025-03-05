7aa6887a35bb12c6406e5278dea3f5df103116ad
exaclior <yushengzhao2020@outlook.com>
2025-03-05 20:50:45 +0800

    fixed unconstrained

--- src/sos_solver.jl
+++ src/sos_solver.jl
@@ -11,12 +11,13 @@ function get_C_α_j(basis::VE, localizing_mtx::VectorConstraint) where {VE<:Abst
     ] for i in 1:3]
 
 	for (i,cur_expr) in enumerate(getfield(localizing_mtx, :func))
-        row_idx, col_idx = mod1(i, dim), min(i ÷ dim + 1, dim)
+        row_idx, col_idx = mod1(i, dim), div(i, dim, RoundUp)
         for (α, coeff) in getfield(cur_expr, :terms)
             α_idx = findfirst(isequal(α), basis)
+            isnothing(α_idx) && continue
 			push!(Is[α_idx], row_idx)
 			push!(Js[α_idx], col_idx)
-			push!(Vs[α_idx], coeff)
+			push!(Vs[α_idx], coeff * (row_idx == col_idx ? 1.0 : 2.0))
 		end
 	end
 
@@ -26,36 +27,42 @@ function get_C_α_j(basis::VE, localizing_mtx::VectorConstraint) where {VE<:Abst
     ]
 end
 
-function dualize(model::Model)
+init_dual_variables(model::Model,dual_model::Model)= [
+		begin
+			G_dim = sdp_constraint |> (x -> getfield(x, :side_dimension)) ∘ JuMP.shape ∘ constraint_object
+			@variable(dual_model, [1:G_dim, 1:G_dim] in PSDCone())
+		end
+		for sdp_constraint in model[:mtx_constraints]
+	]
+
+function dualize(model::Model, total_basis2var_dict::Dict{M,VRef}) where {M<:AbstractMonomialLike,VRef<:AbstractVariableRef}
+
 	dual_model = Model()	
 
+    dual_variables = init_dual_variables(model,dual_model)
 
-    dual_variables = [
-        begin
-            G_dim = sdp_constraint |> (x -> getfield(x, :side_dimension)) ∘ JuMP.shape ∘ constraint_object
-            @variable(dual_model, [1:G_dim, 1:G_dim] in PSDCone())
-        end for sdp_constraint in model[:mtx_constraints]
-    ]
+    dual_model[:dual_variables] = dual_variables
 
 	@variable(dual_model, b)
     @objective(dual_model, Max, b)
 
-
     primal_objective_terms = model |> (x -> getfield(x, :terms)) ∘ objective_function
 
+    symmetric_basis = sort(unique([symmetric_canonicalize(basis) for basis in keys(total_basis2var_dict)]))
 
-    f_α_constraints = [AffExpr(haskey(primal_objective_terms, α) ? primal_objective_terms[α] : 0.0) for α in all_variables(model)]
+	symmetric_variables = getindex.(Ref(total_basis2var_dict), symmetric_basis)
+
+    f_α_constraints = [AffExpr(haskey(primal_objective_terms, α) ? primal_objective_terms[α] : 0.0) for α in symmetric_variables]
 
 	f_α_constraints[1] -= b
 
-    [
-        begin
-            C_α_j = get_C_α_j(all_variables(model), constraint_object(sdp_constraint));
-            f_α_constraints .-= (map(x -> LinearAlgebra.tr(x * dual_variables[i]), C_α_j))
-        end for (i, sdp_constraint) in enumerate(model[:mtx_constraints])
-    ]
+    
+    for (i, sdp_constraint) in enumerate(model[:mtx_constraints])
+        C_α_j = get_C_α_j(symmetric_variables, constraint_object(sdp_constraint))
+        f_α_constraints .-= (map(x -> LinearAlgebra.tr(x * dual_variables[i]), C_α_j))
+    end
 
-    @constraint(dual_model, f_α_constraints in MOI.Zeros(length(all_variables(model))))
+    @constraint(dual_model, f_α_constraints in MOI.Zeros(length(symmetric_basis)))
     return dual_model
 end
 
--- test/sos_solver.jl
+++ test/sos_solver.jl
@@ -1,14 +1,90 @@
 using Test, NCTSSOS, DynamicPolynomials, Clarabel
 using JuMP
 
-@test "Dualization" begin
+using NCTSSOS: get_C_α_j
+using SparseArrays
 
-    n = 3
+@testset "C_α_j" begin
+	model = Model()
+	@variable(model, x[1:4])
+
+    cons = @constraint(model,[x[1] - x[2] x[3] x[4] + x[1]; x[1] - x[2] x[3] x[4] + x[1]; x[1] - x[2] x[3] x[4] + x[1]] in PSDCone())
+
+
+
+    C_α_js = get_C_α_j(x, constraint_object(cons))
+
+    @test C_α_js == [sparse([1, 2, 3, 1, 2, 3], [1, 1, 1, 3, 3, 3], [1.0, 1.0, 1.0, 1.0, 1.0, 1.0], 3, 3),
+	 sparse([1, 2, 3], [1, 1, 1], [-1.0, -1.0, -1.0], 3, 3),
+	 sparse([1, 2, 3], [2, 2, 2], [1.0, 1.0, 1.0], 3, 3),
+	 sparse([1, 2, 3], [3, 3, 3], [1.0, 1.0, 1.0], 3, 3)]
+end
+
+@testset "Dualization Trivial Example 2" begin
+    n = 2
+    true_min = 3
     @ncpolyvar x[1:n]
-    f = x[1]^2 - x[1] * x[2] - x[2] * x[1] + 3x[2]^2 - 2x[1] * x[2] * x[1] + 2x[1] * x[2]^2 * x[1] - x[2] * x[3] - x[3] * x[2] +
-        6x[3]^2 + 9x[2]^2 * x[3] + 9x[3] * x[2]^2 - 54x[3] * x[2] * x[3] + 142x[3] * x[2]^2 * x[3]
 
-	g = sum(x.^2) - 10
+    f = x[1]^2 + x[1] * x[2] + x[2] * x[1] + x[2]^2 + true_min
+    r = -10
+    g1 = r - x[1]
+    g2 = r - x[2]
+    g3 = x[1] - r
+    g4 = x[2] - r
+
+    mom_method = MomentMethod(ceil(Int, maxdegree(f) / 2), identity, x)
+
+    pop = PolynomialOptimizationProblem(f, [g1, g2, g3, g4], x)
+
+    model = make_sdp(mom_method, pop)
+
+    dual_model = dualize(model, get_total_basis2var_dict(mom_method))
+
+	set_optimizer(model, Clarabel.Optimizer)
+	optimize!(model)
+
+	set_optimizer(dual_model, Clarabel.Optimizer)
+	optimize!(dual_model)
+
+	@test is_solved_and_feasible(model)
+	@test is_solved_and_feasible(dual_model)
+
+	@test isapprox(objective_value(model), true_min, atol=1e-6)
+	@test isapprox(objective_value(dual_model), true_min, atol=1e-6)
+end
+
+@testset "Dualization Example 2" begin
+    n = 2
+    @ncpolyvar x[1:n]
+    f = 2-x[1]^2+x[1]*x[2]^2*x[1]-x[2]^2
+    g = 4-x[1]^2-x[2]^2
+    h1 = x[1]*x[2]+x[2]*x[1]-2
+	h2 = -h1
+    pop = PolynomialOptimizationProblem(f, [g, h1, h2], x)
+
+	mom_method = MomentMethod(2, identity, x)
+
+	model = make_sdp(mom_method, pop)
+	dual_model = dualize(model, get_total_basis2var_dict(mom_method))
+
+	set_optimizer(model, Clarabel.Optimizer)
+	optimize!(model)
+
+	set_optimizer(dual_model, Clarabel.Optimizer)
+	optimize!(dual_model)
+
+	@test is_solved_and_feasible(model)
+	@test is_solved_and_feasible(dual_model)
+	@test isapprox(objective_value(model), -1, atol=1e-6)
+	@test isapprox(objective_value(dual_model), -1, atol=1e-6)
+end
+
+@testset "Dualization Trivial Example" begin
+    n = 2
+	true_min = 3
+    @ncpolyvar x[1:n]
+
+    f = x[1]^2 + x[1] * x[2] + x[2] * x[1] + x[2]^2 + true_min
 
     mom_method = MomentMethod(ceil(Int, maxdegree(f) / 2), identity, x)
 
@@ -16,7 +92,45 @@ using JuMP
 
     model = make_sdp(mom_method, pop)
 
-    dual_model = dualize(model)
+    dual_model = dualize(model, get_total_basis2var_dict(mom_method))
+
+    set_optimizer(model, Clarabel.Optimizer)
+    optimize!(model)
+	objective_value(model)
+
+	set_optimizer(dual_model, Clarabel.Optimizer)
+	optimize!(dual_model)
+	objective_value(dual_model)
+
+	dual_model[:dual_variables]
+
+	all_variables(dual_model)
+	value.(all_variables(dual_model))
+
+	all_constraints(dual_model, include_variable_in_set_constraints=true)
+
+    @test is_solved_and_feasible(model)
+    @test is_solved_and_feasible(dual_model)
+    @test isapprox(objective_value(model), true_min, atol=1e-8)
+    @test isapprox(objective_value(dual_model), true_min, atol=1e-8)
+
+end
+
+@testset "Dualization Example 1" begin 
+
+    n = 3
+    @ncpolyvar x[1:n]
+
+    f = x[1]^2 - x[1] * x[2] - x[2] * x[1] + 3x[2]^2 - 2x[1] * x[2] * x[1] + 2x[1] * x[2]^2 * x[1] - x[2] * x[3] - x[3] * x[2] +
+        6x[3]^2 + 9x[2]^2 * x[3] + 9x[3] * x[2]^2 - 54x[3] * x[2] * x[3] + 142x[3] * x[2]^2 * x[3]
+
+    mom_method = MomentMethod(ceil(Int, maxdegree(f) / 2), identity, x)
+
+    pop = PolynomialOptimizationProblem(f, x)
+
+    model = make_sdp(mom_method, pop)
+
+    dual_model = dualize(model,get_total_basis2var_dict(mom_method))
 
     set_optimizer(model, Clarabel.Optimizer)
     optimize!(model)
@@ -24,16 +138,10 @@ using JuMP
 	set_optimizer(dual_model, Clarabel.Optimizer)
 	optimize!(dual_model)
 
-	objective_value(dual_model)
-
-    constraint_object(all_constraints(dual_model, include_variable_in_set_constraints=true)[2]).func[1]
-
-
     @test is_solved_and_feasible(model)
     @test is_solved_and_feasible(dual_model)
     @test isapprox(objective_value(model), 4.372259295498716e-10, atol=1e-8)
     @test isapprox(objective_value(dual_model), 4.372259295498716e-10, atol=1e-8)
-
 end
 
 
