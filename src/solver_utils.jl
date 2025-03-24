2572b9ebb49b688b84793dcbb0f6db9f85c9fe9b
exaclior <yushengzhao2020@outlook.com>
2025-03-24 14:56:53 +0800

    impl tracial

--- src/solver_utils.jl
+++ src/solver_utils.jl
@@ -2,21 +2,33 @@
 # which is not equal to y^2*x^1
 # plue this bug: https://github.com/JuliaAlgebra/DynamicPolynomials.jl/issues/118#issue-1412618512
 # I will keep this function here
-function remove_zero_degree(m::Monomial{C}) where {C}
+function remove_zero_degree(m::Monomial)
     isconstant(m) && return m
     return prod([x[1]^x[2] for x in filter(!(iszero ∘ last), collect(zip(m.vars, m.z)))])
 end
 
-function star(m::Monomial{C}) where {C}
+function star(m::Monomial)
     return prod([x[1]^x[2] for x in filter(!(iszero ∘ last), reverse(collect(zip(m.vars, m.z))))])
 end
 
-function symmetric_canonicalize(monomial::Monomial{C}) where {C}
+function symmetric_canonicalize(monomial::Monomial)
     return min(monomial, star(monomial))
 end
 
-function symmetric_canonicalize(poly::Polynomial{C,T}) where {C,T}
+function symmetric_canonicalize(poly::Polynomial)
     return mapreduce(p -> coefficient(p)' * symmetric_canonicalize(monomial(p)), +, terms(poly); init=zero(poly))
+end
+
+function cyclic_canonicalize(monomial::Monomial)
+    isconstant(monomial) && return monomial
+    return minimum(mapreduce(vcat, 0:length(monomial.vars)-1) do shift
+        shifted_mono = prod([var^expo for (var, expo) in circshift!(collect(zip(monomial.vars, monomial.z)), shift)])
+        [shifted_mono, star(shifted_mono)]
+    end)
+end
+
+function cyclic_canonicalize(poly::Polynomial)
+    return mapreduce(p -> coefficient(p) * cyclic_canonicalize(monomial(p)), +, terms(poly); init=zero(poly))
 end
 
 #NOTE: I did not consider binary variable but it's easy to extend, just filter out in monomial z==2 && vars in binary set
--- test/solver_utils.jl
+++ test/solver_utils.jl
@@ -1,7 +1,7 @@
 using Test, NCTSSOS
 using DynamicPolynomials
 using JuMP
-using NCTSSOS: remove_zero_degree, star, symmetric_canonicalize, get_basis, support, neat_dot, get_dim, _comm, _unipotent, _projective, reduce!
+using NCTSSOS: remove_zero_degree, star, symmetric_canonicalize, get_basis, support, neat_dot, get_dim, _comm, _unipotent, _projective, reduce!, cyclic_canonicalize
 
 @testset "Utilities" begin
     @ncpolyvar x y z
@@ -41,7 +41,6 @@ using NCTSSOS: remove_zero_degree, star, symmetric_canonicalize, get_basis, supp
     end
 
     @testset "Symmetric Canonical Form" begin
-
         mono1 = x^2 * y * z
         mono1_sym = symmetric_canonicalize(mono1)
         @test mono1_sym.vars == [z, y, x]
@@ -85,6 +84,19 @@ using NCTSSOS: remove_zero_degree, star, symmetric_canonicalize, get_basis, supp
         poly3_sym = symmetric_canonicalize(poly3)
 
         @test poly3_sym == mapreduce(v -> v[1] * reduce(*, a[v[2]]), +, zip(coe, supp))
+    end
+
+    @testset "Cyclic Canonical Form" begin
+        @test cyclic_canonicalize(x * y^2 * x) == y^2 * x^2
+        @test cyclic_canonicalize(y*y*z) == z*y^2
+        @test cyclic_canonicalize(z*z) == z^2
+        @test cyclic_canonicalize(one(x)) == 1
+
+        n = 3
+        @ncpolyvar a[1:n]
+        f = a[1]^2 - a[1] * a[2] - a[2] * a[1] + 3a[2]^2 - 2a[1] * a[2] * a[1] + 2a[1] * a[2]^2 * a[1] - a[2] * a[3] - a[3] * a[2] +
+            6a[3]^2 + 9a[2]^2 * a[3] + 9a[3] * a[2]^2 - 54a[3] * a[2] * a[3] + 142a[3] * a[2]^2 * a[3] + 5 * a[1]^2 * a[2]^2 + 5 * a[1] * a[2] * a[3] + 5 * a[3] * a[2] * a[1]
+        @test cyclic_canonicalize(f) == cyclic_canonicalize(7 * a[1] * a[2]^2 * a[1] + 142 * a[3] * a[2]^2 * a[3] - 2 * a[1] * a[2] * a[1] + 18 * a[2]^2 * a[3] - 54 * a[3] * a[2] * a[3] + 1 * a[1]^2 - 2 * a[1] * a[2] + 3 * a[2]^2 - 2 * a[2] * a[3] + 6 * a[3]^2 + 10 * a[1] * a[2] * a[3])
     end
 
     @testset "Get basis" begin
